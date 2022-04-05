723b1156faba021ff124d484c74fa0ee76deca03
Lunjalook <azra.cosickic@gmail.com>
2022-04-05 07:42:22 +0200

    Adding exercise - undefined value

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -4,12 +4,13 @@ function addThree() {
     sum = sum + 3;
     console.log("sum from addThree: " + sum);
 }
-console.log(addThree());
+
 function addFive() {
     sum = sum + 5;
     console.log("sum from addFive: " + sum);
 }
-console.log(addFive());
+addThree();
+addFive();
 
 module.exports = {
     addThree,
